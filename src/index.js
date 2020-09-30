bcf081cebec4fa8d4f6266b477754c0cafbe909f
Helga Maznikava <helga@helgas-air.lan>
2020-09-30 13:09:18 -0700

    feat: add max min avg implementation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  if (array === undefined || array.length === 0) {
+    return 0; 
+  }
+  var min = Infinity;
+  for(var i = 0; i < array.length; i++) {
+    if(array[i] < min) {
+      min = array[i];
+    }
+  }
+  return min;
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+  if (array === undefined || array.length === 0) {
+    return 0; 
+  }
+  var max = -Infinity;
+  for(var i = 0; i < array.length; i ++) {
+    if(array[i] > max) {
+      max = array[i];
+    }
+  }
+  return max;
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  if (array === undefined || array.length === 0) {
+    return 0; 
+  }
+  var avg = 0;
+  for (var i = 0; i < array.length; i++) {
+    avg = avg + array[i];
+  }
+  return (avg / array.length);
+};
